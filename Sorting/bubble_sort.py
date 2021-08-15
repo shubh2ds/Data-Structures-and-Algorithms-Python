ca3db9cff817b0b2bc35707c0ece7f95b03e3df2
Shubham <shubh2ds@gmail.com>
2021-08-15 19:02:44 +0530

    buuble sort is working but need to optimize.

---
+++ Sorting/bubble_sort.py
@@ -0,0 +1,15 @@
+#Bubble Sort
+#Compare adjacent and swap adjacent if L[i+1]<L[i] and move forward till the last element.Finally largest would be moved to the end, Repeat.
+L=[10,8,20,5,22,3]
+
+for j in range(0,len(L)-1): 
+  for i in range(0,len(L)-1):
+    if L[i+1]<L[i]:
+      #swap
+      tmp=L[i]
+      L[i]=L[i+1]
+      L[i+1]=tmp
+    else:
+      continue
+print(L)
+#OUTPUT: [3, 5, 8, 10, 20, 22]
