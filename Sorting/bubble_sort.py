896c951eb2d077fb5ea1b465865aa93603a37886
Shubham <shubh2ds@gmail.com>
2021-08-15 20:11:57 +0530

    Time complexity analysed.Need to optimize.

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -12,4 +12,12 @@ for j in range(0,len(L)-1):
     else:
       continue
 print(L)
+
+#Worst case :if list is unsorted.
+#TC: O(n2) 
+
+#Best case :if list is sorted.
+#TC:O(n2) 
+
+#It is a Stable algorithm: Bubble sort keeps the order of the element same after sorting.
 #OUTPUT: [3, 5, 8, 10, 20, 22]
