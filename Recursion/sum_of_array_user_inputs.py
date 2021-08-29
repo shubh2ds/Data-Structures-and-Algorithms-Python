a96782c93c930d67d42852575a63159b5b4dc174
Shubham Kumar <42023464+shubh2ds@users.noreply.github.com>
2021-08-30 05:24:03 +0530

    def sumArray(arr):

---
+++ Recursion/sum_of_array_user_inputs.py
@@ -0,0 +1,14 @@
+def sumArray(arr):
+    # Please add your code here
+    if len(arr)==0:
+        return 0
+    if len(arr)==1:
+        return arr[0]
+    return arr[0]+sumArray(arr[1:])
+
+# Main
+from sys import setrecursionlimit
+setrecursionlimit(11000)
+n=int(input())
+arr=list(int(i) for i in input().strip().split(' '))
+print(sumArray(arr))
