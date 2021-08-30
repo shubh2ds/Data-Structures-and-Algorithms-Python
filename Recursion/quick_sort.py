0d1a8e8ea7a5f0af47c9a1c8911aeb0b4d351e15
Shubham Kumar <42023464+shubh2ds@users.noreply.github.com>
2021-08-30 21:59:04 +0530

    pivot index calculated,partition performed followed by quick sort using recursion.

---
+++ Recursion/quick_sort.py
@@ -0,0 +1,41 @@
+def partition_for_quick_sort(arr,sidx,eidx):
+  pivot=arr[sidx]
+  c=0
+  for i in range(sidx,eidx+1):
+    if arr[i]<pivot:
+      c=c+1
+  arr[sidx+c],arr[sidx] = arr[sidx],arr[sidx+c]
+  pivot_idx=sidx+c
+  i=sidx
+  j=eidx
+  while i<j:
+    if arr[i]<pivot:
+      i=i+1
+    elif arr[j]>=pivot:
+      j=j-1
+    else:
+      arr[i],arr[j]=arr[j],arr[i]
+      i=i+1
+      j=j-1
+  return pivot_idx
+"""
+arr=[6,2,1,4,3,8,9,12,5]
+sidx,eidx=0,len(arr)-1
+partition(arr,sidx,eidx)
+print(arr)
+"""
+
+
+def quick_sort(arr,sidx,eidx):
+  if len(arr)==0 or len(arr)==1:
+    return
+  if sidx > eidx:
+    return
+  pivot_idx=partition_for_quick_sort(arr,sidx,eidx)
+  quick_sort(arr,sidx,pivot_idx-1)
+  quick_sort(arr,pivot_idx+1,eidx)
+
+arr=[6,2,1,4,3,8,9,12,5]
+sidx,eidx=0,len(arr)-1
+quick_sort(arr,sidx,eidx)
+print(arr)
