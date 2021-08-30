e928739ee6d0494208c8e7cf3446104710f5eff5
Shubham Kumar <42023464+shubh2ds@users.noreply.github.com>
2021-08-30 16:16:32 +0530

    Remove matched character in the string done using recursion.

---
+++ Recursion/string_remove_matched_char.py
@@ -0,0 +1,12 @@
+# Problem: Remove x from string
+def removeX(string): 
+    if len(string)==0:
+        return string
+    if string[0]=="x":
+        return removeX(string[1:])
+    else:
+        return string[0]+removeX(string[1:])
+
+# Main
+string = input()
+print(removeX(string))
