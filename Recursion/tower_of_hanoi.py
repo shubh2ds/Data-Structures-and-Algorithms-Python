7695eaabdac0056d389a96f7d89b0bee2cd75bde
Shubham Kumar <42023464+shubh2ds@users.noreply.github.com>
2021-08-30 22:26:49 +0530

    Tower of hanoi using recursion.

---
+++ Recursion/tower_of_hanoi.py
@@ -0,0 +1,11 @@
+def towerofhanoi(n, source, aux, dest):
+    # Please add your code here
+    if n==1:
+        print(source,"",dest)
+        return
+    towerofhanoi(n-1, source, dest, aux)
+    print(source,"",dest)
+    towerofhanoi(n-1, aux, source, dest)
+
+n=int(input())
+towerofhanoi(n, 'a', 'b', 'c')
