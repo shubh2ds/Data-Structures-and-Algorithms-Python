412479ed5e270cc71c3cd014e35026945c64fe93
Shubham <shubh2ds@gmail.com>
2024-07-25 18:07:47 +0530

    script to remove leaf node

--- Tree/07.remove_leaf_nodes.py
+++ Tree/07.remove_leaf_nodes.py
@@ -1,1 +1,46 @@
-print()
+class BinaryTreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def printTreeDetailed(root):
+    if root == None:
+        return 
+    print(root.data, end=':')
+
+    if root.left !=None:
+        print(root.left.data,end=' , ')
+    if root.right !=None:
+        print(root.right.data,end='')
+    print()
+
+    printTreeDetailed(root.left)
+    printTreeDetailed(root.right)
+
+def inputToTree():
+    n = int(input("Enter node value:"))
+    if n == -1:
+        return None
+    root = BinaryTreeNode(n)
+    root.left = inputToTree()
+    root.right = inputToTree()
+    
+    return root
+
+def removeLeafNode(root):
+    if root == None:
+        return None
+    if root.left ==None and root.right == None:
+        return None
+
+    root.left = removeLeafNode(root.left)
+    root.right = removeLeafNode(root.right)
+    return root
+
+root = inputToTree()
+print("Tree:")
+printTreeDetailed(root)
+root = removeLeafNode(root)
+print("Tree after removing leaves:")
+printTreeDetailed(root)
