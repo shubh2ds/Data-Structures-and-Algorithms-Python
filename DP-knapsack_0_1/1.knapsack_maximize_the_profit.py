3d577eddb423b7786c064fd3c81bb123c1a12d46
Shubham <shubh2ds@gmail.com>
2024-08-07 02:26:06 +0530

    select the items for the bag to maximize the profit in terms of value.

--- DP-knapsack_0_1/1.knapsack_maximize_the_profit.py
+++ DP-knapsack_0_1/1.knapsack_maximize_the_profit.py
@@ -4,18 +4,19 @@ val = [1,4,5,7]    # item price/value
 w = 7 # bag capacity
 
 #choice diagram:
-#
+# wt[n-1] <= w then item will be picked
+# wt[n-1] > w  then item will not be picked, bcoz no space in bag
 def knapsack(wt, val, w, n):
     if n == 0  or w == 0:
         return 0
-    else:
-        if wt[n-1] <= w:
-            return max(
-                ( val[n-1] + knapsack(wt, val, w-wt[n-1], n-1) ) , knapsack(wt, val, w, n-1)
+    
+    if wt[n-1] <= w:
+        return max(
+            ( val[n-1] + knapsack(wt, val, w-wt[n-1], n-1) ) , knapsack(wt, val, w, n-1)
 
-            )
-        elif wt[n-1] > w :
-            return knapsack(wt, val, w, n-1)
+        )
+    elif wt[n-1] > w :
+        return knapsack(wt, val, w, n-1)
 
 
 output = knapsack(wt, val, w, n)
