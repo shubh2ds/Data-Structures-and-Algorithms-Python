618ba73d571c5a492de2cb0049ae8785a1623057
Shubham <shubh2ds@gmail.com>
2024-08-07 02:47:05 +0530

    if subset sum possible to the given sum or not

--- DP-knapsack_0_1/3.subset_sum.py
+++ DP-knapsack_0_1/3.subset_sum.py
@@ -1,3 +1,23 @@
-arr = [2,3,7,8,10]
-sum = 11
+# [2,3,5,6,8,10] , n = 6 , sum = 10
+wt  = val = arr = [2,3,7,8,10]  
+n = 5
+w = sum = 11
 #OP: True/False # {3,8}
+
+def knapsack(wt, val, w, n):
+    if n == 0  or w == 0:
+        return 0
+    
+    if wt[n-1] <= w:
+        return max(( val[n-1] + knapsack(wt, val, w-wt[n-1], n-1) ) , knapsack(wt, val, w, n-1))
+    
+    elif wt[n-1] > w :
+        return knapsack(wt, val, w, n-1)
+
+
+output = knapsack(wt, val, w, n)
+print("max profit:",output)
+if output == sum:
+    print(True)
+else:
+    print(False)
