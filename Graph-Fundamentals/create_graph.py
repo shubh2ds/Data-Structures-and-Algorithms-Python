b2348057d3aaa91b312e72fde2cf27a696a8efc0
Shubham <shubh2ds@gmail.com>
2024-08-12 22:28:42 +0530

    graph created with vertex and edges

--- Graph-Fundamentals/create_graph.py
+++ Graph-Fundamentals/create_graph.py
@@ -11,6 +11,12 @@ class Graph:
         for vertex in self.adj_list.keys():
             print(vertex,":",self.adj_list[vertex])
             print()
+    def add_edge(self,V1,V2):
+        if V1 in self.adj_list.keys() and V2 in self.adj_list.keys():
+            self.adj_list[V1].append(V2)
+            self.adj_list[V2].append(V1)
+            return True
+        return False
 
 
 obj = Graph()
@@ -19,6 +25,14 @@ obj.add_vertex('B')
 obj.add_vertex('C')
 obj.add_vertex('D')
 obj.add_vertex('E')
+print("Vertex added!")
+
+obj.add_edge('A','B')
+obj.add_edge('B','C')
+obj.add_edge('C','D')
+obj.add_edge('D','E')
+obj.add_edge('E','A')
+print("Edges Connected!")
 obj.print_graph()
 
 
