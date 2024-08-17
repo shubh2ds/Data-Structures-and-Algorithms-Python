a7114df8dd7cc89e704ee33176141c4aad90e5b1
Shubham <shubh2ds@gmail.com>
2024-08-18 01:46:30 +0530

    linked list remove by index operations added

--- Linked-List-Fundamentals/linkedlist.py
+++ Linked-List-Fundamentals/linkedlist.py
@@ -52,6 +52,24 @@ class LinkedList:
             if self.length == 0:
                 self.tail = None
             return tmp.value
+    def pop(self):
+        if self.length == 0 :
+            return False
+        if self.length == 1 :
+            self.head = None
+            self.tail = None
+            self.length = 0
+            return True
+        tmp = self.head
+        for _ in range(self.length-1):
+            tmp = tmp.next
+        tmp.next = None
+        tmp = None
+
+        self.length -= 1
+        return True
+        
+
     def get(self,index):
         if index<0 or index >= self.length:
             return None
@@ -81,6 +99,18 @@ class LinkedList:
         new_node= Node(value)
         new_node.next = tmp.next
         tmp.next = new_node
+        return True
+    def remove(self,index):
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            self.pop_first()   
+        if index == self.length:
+            self.pop()
+        tmp = self.head
+        for _ in range(index-1):
+            tmp = tmp.next
+        tmp.next = tmp.next.next
         return True
 
     def print(self):
@@ -112,4 +142,9 @@ linked_list.set(3,100)
 print("get after set of 100 at 3:", linked_list.get(3))
 print("insert 1000 at 3rd index.")
 linked_list.insert(3,1000)
+linked_list.print()
+print("pop:",linked_list.pop())
+#print("pop:",linked_list.pop())
+linked_list.print()
+print("remove:",linked_list.remove(3))
 linked_list.print()
