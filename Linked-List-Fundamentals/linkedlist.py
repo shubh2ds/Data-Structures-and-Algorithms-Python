6400a9d933c096f07dd05a0c0d5925b54c2704f5
Shubham <shubh2ds@gmail.com>
2024-08-18 01:29:13 +0530

    linked list operations added

--- Linked-List-Fundamentals/linkedlist.py
+++ Linked-List-Fundamentals/linkedlist.py
@@ -10,14 +10,79 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-    def append(self,value):
-        pass
+    def append1(self,value):
+        if self.head == None:
+            return Node(value)
+        tmp = self.head
+        while tmp.next is not None:
+            tmp = tmp.next
+        tmp.next = Node(value)
+        #return tmp
+        #pass
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
 
     def prepend(self,value):
-        pass
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            tmp = self.head
+            self.head = new_node
+            new_node.next = tmp
+        self.length += 1
+    def pop_first(self):
+        if self.length == 0 :
+            return None
+        else:
+            tmp = self.head
+            self.head = self.head.next
+            tmp.next = None
+            self.length -= 1
+            if self.length == 0:
+                self.tail = None
+            return tmp.value
+    def get(self,index):
+        if index<0 or index >= self.length:
+            return None
+        tmp = self.head
+        for _ in range(0,index):
+            tmp = tmp.next
+        return tmp.value
+    def set(self,index,value):
+        if index<0 or index >= self.length:
+            return False
+        tmp = self.head
+        for _ in range(index):
+            tmp = tmp.next
+        tmp.value = value
+        return True
 
     def insert(self,index,value):
-        pass
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            self.prepend(value)
+        if index == self.length:
+            self.append(value)
+        tmp = self.head
+        for _ in range(index-1):
+            tmp = tmp.next
+        new_node= Node(value)
+        new_node.next = tmp.next
+        tmp.next = new_node
+        return True
+
     def print(self):
         if self.head == None:
             return None
@@ -29,4 +94,22 @@ class LinkedList:
 
 linked_list = LinkedList(4)
 #linked_list = LinkedList(5)
+linked_list.append(5)
+linked_list.append(6)
+linked_list.append(7)
+linked_list.print()
+print("after prepend")
+linked_list.prepend(70)
+linked_list.prepend(60)
+linked_list.prepend(50)
+linked_list.print()
+print("pop first")
+print(linked_list.pop_first())
+print("after pop")
+linked_list.print()
+print("get at 3:", linked_list.get(3))
+linked_list.set(3,100)
+print("get after set of 100 at 3:", linked_list.get(3))
+print("insert 1000 at 3rd index.")
+linked_list.insert(3,1000)
 linked_list.print()
